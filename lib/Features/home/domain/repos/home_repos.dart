db6dba56fd567aa883b5a64ce676e22d43ef96c5
mecodeur <mehdi.el2023@icloud.com>
2023-06-05 16:08:29 +0100

    Refactor HomeRepo add Either

--- lib/Features/home/domain/repos/home_repos.dart
+++ lib/Features/home/domain/repos/home_repos.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 import '../entities/book_entity.dart';
 
 abstract class HomeRepo{
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewsetBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsetBooks();
 }
