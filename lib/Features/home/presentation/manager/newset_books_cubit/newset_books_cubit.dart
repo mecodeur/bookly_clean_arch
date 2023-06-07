6f7d37c39c37c5114275031e33b310445eaebf3f
mecodeur <mehdi.el2023@icloud.com>
2023-06-07 09:55:12 +0100

    create newset book cubit

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/domain/use_cases/featch_newset_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 import '../../../domain/entities/book_entity.dart';
@@ -6,5 +7,18 @@ import '../../../domain/entities/book_entity.dart';
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.featchedNewsetBooksUseCase)
+      : super(NewsetBooksInitial());
+
+  final FeatchedNewsetBooksUseCase featchedNewsetBooksUseCase;
+
+  Future<void> featchedNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await featchedNewsetBooksUseCase.call();
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.message));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_state.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_state.dart
@@ -4,12 +4,15 @@ part of 'newset_books_cubit.dart';
 abstract class NewsetBooksState {}
 
 class NewsetBooksInitial extends NewsetBooksState {}
+
 class NewsetBooksLoading extends NewsetBooksState {}
+
 class NewsetBooksFailure extends NewsetBooksState {
   final String errMessage;
 
   NewsetBooksFailure(this.errMessage);
 }
+
 class NewsetBooksSuccess extends NewsetBooksState {
   final List<BookEntity> books;
 
