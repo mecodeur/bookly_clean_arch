3cce658ee2588f64a6b6ff02b54f23e8411a1c04
mecodeur <mehdi.el2023@icloud.com>
2023-07-10 15:29:43 +0100

    Pagination part 3 : Add a scrollController

--- lib/Features/home/domain/repos/home_repos.dart
+++ lib/Features/home/domain/repos/home_repos.dart
@@ -4,6 +4,8 @@ import 'package:dartz/dartz.dart';
 import '../entities/book_entity.dart';
 
 abstract class HomeRepo {
+
+  // Ma function maintenant accept un Parametre avec une valeur par defaut 0
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks(
       {int pageNumber = 0});
 
--- lib/core/use_cases/use_case.dart
+++ lib/core/use_cases/use_case.dart
@@ -3,6 +3,7 @@ import 'package:dartz/dartz.dart';
 import '../errors/failure.dart';
 
 abstract class UseCase<Type, Param> {
+  // the square brackets [] indicate that the Param param parameter is optional.
   Future<Either<Failure, Type>> call([Param param]);
 }
 
